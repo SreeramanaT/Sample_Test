4a5e4c5b049f120648af784f75a1e6d2e6dc198b
SreeramanaT <60499725+SreeramanaT@users.noreply.github.com>
2020-02-14 15:09:33 +0530

    adding maven project
    adding maven project

---
+++ srtBatch2/src/main/java/com/broadridge/srtBatch2/App.java
@@ -0,0 +1,17 @@
+package com.broadridge.srtBatch2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! from srtBatch2" );
+    }
+    
+    public String returnName() {
+    	return "SRT";
+    }
+}
